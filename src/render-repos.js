89f8a6d902075433b8749edb99a553f61bf4f410
sayed94h <skazimi951@yahoo.com>
2020-10-01 15:13:49 +0200

    render repos is done

---
+++ src/render-repos.js
@@ -0,0 +1,23 @@
+const linksContainer = document.getElementById("link-container");
+
+let getData = function (data) {
+	return data.json();
+};
+
+let renderEachRepo = function (repo) {
+	for (let i = 0; i < repo.length; i++) {
+		let aEl = document.createElement("a");
+		aEl.href = `${repo[i].html_url}`;
+		aEl.innerHTML = `${repo[i].name}<br/><br/>`;
+		aEl.target = "_blank";
+
+		linksContainer.appendChild(aEl);
+	}
+};
+
+fetch("https://api.github.com/users/Sayed94h/repos")
+	.then(getData)
+	.then(renderEachRepo)
+	.catch((msg) => {
+		console.log("Error message: ", msg);
+	});
